4bcfbf356102410aa17f8757571d5abf4661f956
crystal <cheesedrone@gmail.com>
2025-09-30 00:25:50 +0200

    about me page + jewel pixel art gifs + bug fixes (assets and filepaths)
    - got assets to load on the about me page, added spinning jewels, basic content added to the about me page, still need to organize bc the about me page is a MESS :(

--- cursor-bg.js
+++
@@ -1,9 +0,0 @@
-/*document.addEventListener('mousemove', (e) => {
-    const { innerWidth, innerHeight } = window;
-    //  offsets (-50px to +50px)
-    const moveX = ((e.clientX / innerWidth) - 0.5) * 100;
-    const moveY = ((e.clientY / innerHeight) - 0.5) * 100;
-    // update background position of body
-    document.body.style.backgroundPosition = `calc(50% + ${moveX}px) calc(50% + 2*${moveY}px)`;
-}); 
-*/
